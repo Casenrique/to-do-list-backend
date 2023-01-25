ac94c8baf28698e36c79b126411351ec4bd17dba
Casenrique <carloshenriquesouza.eng@gmail.com>
2023-01-25 10:12:35 -0300

    Implementados endpoint Get All Users com query params.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
 import { db } from './database/knex'
+import { serialize } from 'v8'
 
 const app = express()
 
@@ -15,6 +16,34 @@ app.get("/ping", async (req: Request, res: Response) => {
     try {
         const result = await db("users")
         res.status(200).send({ message: "Pong!", result })
+    } catch (error) {
+        console.log(error)
+
+        if (req.statusCode === 200) {
+            res.status(500)
+        }
+
+        if (error instanceof Error) {
+            res.send(error.message)
+        } else {
+            res.send("Erro inesperado")
+        }
+    }
+})
+
+app.get("/users", async (req: Request, res: Response) => {
+    try {
+
+        const searchTerm = req.query.q as string | undefined
+
+        if(searchTerm === undefined) {
+            const result = await db("users")
+            res.status(200).send(result)
+        } else {
+            const result = await db("users").where("name", "LIKE", `%${searchTerm}%`)
+            res.status(200).send(result)
+        }
+
     } catch (error) {
         console.log(error)
 
