322609247cfc65a074e80107ffb0f8cf449d6b91
Casenrique <carloshenriquesouza.eng@gmail.com>
2023-01-25 13:37:07 -0300

    feat: POST tasks implementado.

--- src/types.ts
+++ src/types.ts
@@ -4,3 +4,10 @@ export type TUserDB = {
     email: string,
     password: string
 }
+export type TTaskDB = {
+    id: string,
+    title: string,
+    description: string,
+    created_at: string,
+    status: number
+}
